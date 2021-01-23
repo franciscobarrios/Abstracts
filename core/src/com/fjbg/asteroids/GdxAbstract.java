9bb78daf386ec639eeadd8564c757ec1461f464d
Francisco Barrios <francisco.barrios@ifyoucan.com>
2021-01-23 11:41:50 +0700

    WIP

--- core/src/com/fjbg/asteroids/GdxAbstract.java
+++ core/src/com/fjbg/asteroids/GdxAbstract.java
@@ -2,34 +2,48 @@ package com.fjbg.asteroids;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.fjbg.asteroids.objects.Connector;
-import com.fjbg.asteroids.objects.Star;
+import com.fjbg.asteroids.objects.Node;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.UUID;
+
+import static com.fjbg.asteroids.common.CommonsKt.CONNECT_DISTANCE;
+import static com.fjbg.asteroids.common.CommonsKt.OBJECTS;
 
 public class GdxAsteroids extends ApplicationAdapter {
     private OrthographicCamera camera;
     private ShapeRenderer shape;
-    private ArrayList<Star> stars;
+    private ArrayList<Node> nodes;
+    private SpriteBatch batch;
+    private BitmapFont font;
 
     @Override
     public void create() {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         shape = new ShapeRenderer();
-        stars = new ArrayList<>();
+        nodes = new ArrayList<>();
+        batch = new SpriteBatch();
+
+        font = new BitmapFont();
+
 
         Random random = new Random();
-        for (int i = 0; i < 200; i++) {
-            stars.add(
-                    new Star(
+        for (int i = 0; i < OBJECTS; i++) {
+            nodes.add(
+                    new Node(
+                            UUID.randomUUID().toString(),
                             random.nextInt(Gdx.graphics.getWidth()),
                             random.nextInt(Gdx.graphics.getHeight()),
-                            random.nextInt(12) + 8,
+                            random.nextInt(5) + 2,
                             random.nextInt(2) + 1,
                             random.nextInt(2) + 1
                     )
@@ -42,28 +56,37 @@ public class GdxAsteroids extends ApplicationAdapter {
     public void render() {
         Gdx.gl.glEnable(GL20.GL_BLEND);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
-
         shape.setProjectionMatrix(camera.combined);
         shape.begin(ShapeRenderer.ShapeType.Filled);
 
-        Connector connector = new Connector();
-        for (int i = 0; i < stars.size(); i++) {
-            stars.get(i).update();
-            stars.get(i).draw(shape);
-            for (int j = 0; j < stars.size(); j++) {
-                float d = connector.distanceBetweenStars(stars.get(i), stars.get(j));
-                if (d <= 300) {
+        batch.begin();
+        font.setColor(Color.WHITE);
 
-                    connector.connect(stars.get(i), stars.get(j), shape);
+
+        Connector connector = new Connector();
+        for (int i = 0; i < nodes.size(); i++) {
+            nodes.get(i).update();
+            nodes.get(i).drawWithGlow(shape);
+
+            font.draw(batch, nodes.get(i).getId(), 500, 500);
+
+
+            for (int j = 0; j < nodes.size(); j++) {
+                float d = connector.distanceBetweenStars(nodes.get(i), nodes.get(j));
+                if (d <= CONNECT_DISTANCE) {
+                    connector.connect(nodes.get(i), nodes.get(j), shape);
+                    nodes.get(i).handleList(nodes.get(j));
                 }
             }
+
+            //System.out.println(" list size: " + nodes.get(i).getList().size());
         }
         shape.end();
+        batch.end();
     }
 
     @Override
---
+++ core/src/com/fjbg/asteroids/common/Commons.kt
@@ -0,0 +1,2 @@
+package com.fjbg.asteroids.common
+
