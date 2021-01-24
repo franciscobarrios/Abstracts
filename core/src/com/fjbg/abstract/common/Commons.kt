cb46ad7c8bcd023089eef44b034b6d41821f10ed
Francisco Barrios <francisco.barrios@ifyoucan.com>
2021-01-24 13:55:57 +0700

    WIP - Core: background music fixed - Project: image folder added

--- core/src/com/fjbg/abstract/common/Commons.kt
+++ core/src/com/fjbg/abstract/common/Commons.kt
@@ -3,7 +3,7 @@ package com.fjbg.abstract.common
 import com.badlogic.gdx.graphics.Color
 
 const val CONNECT_DISTANCE = 400
-const val OBJECTS = 20
+const val OBJECTS = 100
 
 fun objectColor(alpha: Float): Color {
 	return Color(0f, 1f, 1f, alpha)
